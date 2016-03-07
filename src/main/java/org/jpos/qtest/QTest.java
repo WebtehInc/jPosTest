ef923675b0c7c3178db6f15b0679fffc70429053
Borna Novak <dosadnizub@gmail.com>
2016-03-07 14:14:40 +0100

    better, more streamlined version using the defaults

--- src/main/java/org/jpos/qtest/QTest.java
+++ src/main/java/org/jpos/qtest/QTest.java
@@ -2,47 +2,21 @@ package org.jpos.qtest;
 
 import org.jpos.iso.ISOUtil;
 import org.jpos.q2.*;
-import org.jpos.util.Log;
 
 public class QTest extends QBeanSupport implements Runnable, QTestMBean {
-    volatile int state;
-    long tickInterval = 1000;
-    Log log;
-
-    public QTest() {
-        super();
-        state = -1;
-        log = Log.getLog(Q2.LOGGER_NAME, "qtest");
-        log.info("constructor");
-    }
-
-    public void init() {
-        log.info("init");
-        state = STARTING;
-    }
-
-    public void start() {
-        log.info("start");
-        state = STARTED;
-        new Thread(this).start();
-    }
-
-    public void stop() {
-        log.info("stop");
-        state = STOPPING;
-    }
-
-    public void destroy() {
-        log.info("destroy");
-        state = STOPPED;
+    long tickInterval;
+    
+    public long getTickInterval() {
+        return tickInterval;
     }
 
     public void setTickInterval(long tickInterval) {
         this.tickInterval = tickInterval;
     }
 
-    public long getTickInterval() {
-        return tickInterval;
+    @Override
+    protected void startService() {
+        new Thread(this).start();
     }
 
     public void run() {
@@ -51,17 +25,4 @@ public class QTest extends QBeanSupport implements Runnable, QTestMBean {
             ISOUtil.sleep(tickInterval);
         }
     }
-
-    public int getState() {
-        return state;
-    }
-
-    public String getStateAsString() {
-        return state >= 0 ? stateString[state] : "Unknown";
-    }
-
-    public boolean running() {
-        return state == QBean.STARTING || state == QBean.STARTED;
-    }
-
 }
--- src/main/java/org/jpos/qtest/QTestMBean.java
+++ src/main/java/org/jpos/qtest/QTestMBean.java
@@ -4,4 +4,5 @@ public interface QTestMBean extends org.jpos.q2.QBeanSupportMBean {
     public void setTickInterval(long tickInterval);
 
     public long getTickInterval();
+    
 }
